1a2d4ac7bde71ee500093082510e8df05bfbc4ba
juan-antonio <juanantoniomtzfns@gmail.com>
2025-04-24 19:22:09 +0200

    fix(infrastructure): change return type in getById

--- itx-mobile-shop/src/app/infrastructure/http/product-api.service.ts
+++ itx-mobile-shop/src/app/infrastructure/http/product-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from '../../domain/models/product.model';
+import { Product, ProductDetail } from '../../domain/models/product.model';
 import { environment } from '../../../environments/environment';
 
 const API_BASE_URL = environment.apiBaseUrl;
@@ -17,7 +17,7 @@ export class ProductApiService {
     return this.http.get<Product[]>(`${API_BASE_URL}/product`);
   }
 
-  getById(id: string): Observable<Product> {
-    return this.http.get<Product>(`${API_BASE_URL}/product/${id}`);
+  getById(id: string): Observable<ProductDetail> {
+    return this.http.get<ProductDetail>(`${API_BASE_URL}/product/${id}`);
   }
 }
