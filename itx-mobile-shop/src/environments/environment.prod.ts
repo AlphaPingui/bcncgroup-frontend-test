ca05fed87f1c1b0ef04f5ad4844ba88033cc5446
juan-antonio <juanantoniomtzfns@gmail.com>
2025-04-24 15:07:49 +0200

    chore(environment): add environment configuration for API base URL

---
+++ itx-mobile-shop/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://itx-frontend-test.onrender.com/api'
+  };
+  
---
+++ itx-mobile-shop/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'https://itx-frontend-test.onrender.com/api'
+  };
+  
