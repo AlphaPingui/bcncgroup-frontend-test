c9e153b905d93181ad3f7f61ea2f9ebecf8e8705
juan-antonio <juanantoniomtzfns@gmail.com>
2025-04-24 21:25:35 +0200

    feat(page): create ProductListPage

---
+++ itx-mobile-shop/src/app/pages/product-list-page/product-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-product-list-page',
+  templateUrl: './product-list-page.component.html',
+  styleUrls: ['./product-list-page.component.scss'],
+  imports: [CommonModule]
+})
+
+export class ProductListPageComponent {}
