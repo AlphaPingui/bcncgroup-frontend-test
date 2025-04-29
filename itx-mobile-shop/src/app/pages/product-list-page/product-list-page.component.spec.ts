4db5d0bb2531036ec01f90b1496c1e76b5016dcf
juan-antonio <juanantoniomtzfns@gmail.com>
2025-04-29 16:32:02 +0200

    test: updated ProductListPageComponent test

--- itx-mobile-shop/src/app/pages/product-list-page/product-list-page.component.spec.ts
+++ itx-mobile-shop/src/app/pages/product-list-page/product-list-page.component.spec.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { Product } from '../../domain/models/product.model';
 
 describe('ProductListPageComponent', () => {
     let component: ProductListPageComponent;
@@ -19,11 +21,7 @@ describe('ProductListPageComponent', () => {
                     provide: ActivatedRoute,
                     useValue: {
                         params: of({}),
-                        snapshot: {
-                            paramMap: {
-                                get: () => null,
-                            }
-                        }
+                        snapshot: { paramMap: { get: () => null } }
                     }
                 }
             ],
@@ -39,4 +37,45 @@ describe('ProductListPageComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should render a list of products', () => {
+        spyOn(component, 'filteredProducts').and.returnValue([
+            { id: 'cGjFJlmqNPIwU59AOcY8H', brand: 'Acer', model: 'Liquid 1', imgUrl: 'url1.jpg', price: 100 },
+            { id: 'cGjFJlmqNPIwU59AOcY8J', brand: 'Acer', model: 'Liquid 2', imgUrl: 'url2.jpg', price: 200 }
+        ]);
+
+        fixture.detectChanges();
+
+        const productCards = fixture.debugElement.queryAll(By.css('app-product-card'));
+
+        expect(productCards.length).toBe(2);
+    });
+
+    it('should update searchTerm when user types in search input', () => {
+        const inputElement = fixture.debugElement.query(By.css('input[name="search"]')).nativeElement;
+      
+        inputElement.value = 'iphone';
+        inputElement.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+      
+        expect(component.searchTerm()).toBe('iphone');
+      });
+      
+      it('should filter products based on searchTerm', () => {
+        const mockProducts = [
+            { id: 'cGjFJlmqNPIwU59AOcY8H', brand: 'Acer', model: 'Liquid 1', imgUrl: 'url1.jpg', price: 100 },
+            { id: 'cGjFJlmqNPIwU59AOcY8J', brand: 'Acer', model: 'Liquid 2', imgUrl: 'url2.jpg', price: 200 },
+            { id: 'cGjFJlmqNPIwU59AOcY8L', brand: 'Apple', model: 'iPhone 14', imgUrl: 'url3.jpg', price: 300 }
+        ];
+      
+        component.products.set(mockProducts);
+        component.searchTerm.set('iphone');
+        fixture.detectChanges();
+      
+        const filtered = component.filteredProducts();
+      
+        expect(filtered.length).toBe(1);
+        expect(filtered[0].model).toBe('iPhone 14');
+      });
+      
 });
